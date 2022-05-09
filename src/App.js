b7c1701d8e0963768da3d2c899fb08e721c91fbd
suprava padhi <supravapadhi92@gmail.com>
2022-05-09 20:58:23 +0530

    react routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Fetch from "./components/API_TEST/Fetch"
 import PostMethod from "./components/API_TEST/PostMethod"
+import React_Router from './components/API_TEST/Routing/React_Router'
 function App() {
   return (
     <div>
       {/* <Fetch/> */}
-      <PostMethod/>
+      {/* <PostMethod/> */}
+      <React_Router/>
     </div>
   )
 }
